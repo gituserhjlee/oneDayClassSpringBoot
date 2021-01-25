7e8a306873ead917f543d58208f081d63cfe1f92
Lee Hyeonji <lhj5426@naver.com>
2021-01-25 15:13:07 +0900

    order 제출

--- src/main/java/com/example/security/controller/OrderController.java
+++ src/main/java/com/example/security/controller/OrderController.java
@@ -1,11 +1,16 @@
 package com.example.security.controller;
 
 import com.example.security.entity.Item;
+import com.example.security.entity.user.User;
 import com.example.security.service.ItemService;
+import com.example.security.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderController {
     private final ItemService itemService;
+    private final OrderService orderService;
 
     @GetMapping("/user/buy")
     public String createForm(Model model){
@@ -21,6 +27,13 @@ public class OrderController {
 
         return "orderForm";
 
+    }
+    @PostMapping("/user/buy")
+    public String order(@RequestParam("itemId") Long itemId){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = user.getCode();
+        orderService.order(userId, itemId);
+        return "redirect:/";
     }
 
 
