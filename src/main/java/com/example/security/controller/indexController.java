3ccd504ea6014a1fc5d93229fe37b88823ecc4c6
Lee Hyeonji <lhj5426@naver.com>
2021-01-26 14:18:51 +0900

    user에게는 관리자 사이트 이동 버튼안뜨게

--- src/main/java/com/example/security/controller/indexController.java
+++ src/main/java/com/example/security/controller/indexController.java
@@ -1,8 +1,10 @@
 package com.example.security.controller;
 
 import com.example.security.entity.Item;
+import com.example.security.entity.user.User;
 import com.example.security.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,9 @@ public class indexController {
     @GetMapping("/")
     public String list(Model model){
         List<Item> items=itemService.findItems();
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String userAuth = user.getAuth();
+        model.addAttribute("userAuth",userAuth);
         model.addAttribute("items",items);
         return "main";
     }
